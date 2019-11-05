8a8da1c426a1215261f0fa5490264db9f3dc90a2
Josef Mayor <josefmayor@Josefs-MacBook-Air.local>
2019-11-05 10:26:06 -0800

    Added comment explaining creation of fish_data_cat object and the new lenght column (big/small, >300/<300)

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -5,5 +5,7 @@ fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
 #building a history
 
 library(dplyr)
+#2) this creates a new data frame from the fish_data frame while adding a new column
+#for catfish length with "big" length (>300) and "small" length (<300)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
